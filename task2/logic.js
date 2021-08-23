0abaf6b1f5cdcd215a04559fece4b968f2ef47b1
ItWarior <Tarasbennet@gmail.com>
2021-08-23 13:53:58 +0300

    chek

--- task2/logic.js
+++ task2/logic.js
@@ -1,17 +1,17 @@
 const fs = require('fs/promises');
 const path = require('path');
 
-const way_to_file_users = path.join(__dirname, 'static', 'dbusers.json');
+const WAY_TO_FILE_USERS = path.join(__dirname, 'dbusers.json');
 
 async function getUsers() {
-   const users = await fs.readFile(way_to_file_users);  
+   const users = await fs.readFile(WAY_TO_FILE_USERS);
    const parse_users = (!users.toString()) ? [] : JSON.parse(users);
 
    return parse_users;
 }
 
 async function addUser(users) {
-   await fs.writeFile(way_to_file_users, JSON.stringify(users));
+   await fs.writeFile(WAY_TO_FILE_USERS, JSON.stringify(users));
 }
 
 module.exports = {
